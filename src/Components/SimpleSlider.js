19ace74f11d2e72ea1ed105a0fc6d4ae0f537e1e
vietnh6 <1712906@student.hcmus.edu.vn>
2020-11-09 09:21:41 +0700

    Rename css

--- src/Components/SimpleSlider.js
+++ src/Components/SimpleSlider.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Slider from "react-slick"; 
-import "./taolao.css"
+import "./simpleslide.css"
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import CustomCard from "./CustomCard";
